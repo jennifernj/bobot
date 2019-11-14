82266f00208d1b71c73936bdc795a10b0275b10d
jennifernj <jenniferneda@gmail.com>
2019-11-13 20:40:25 -0800

    Update coil.ino

--- coil.ino
+++ coil.ino
@@ -3,17 +3,15 @@ int RPWM = 6;
 int REN = 10;
 int timeA = 5000;      //duration to run
 void setup() {
-  // put your setup code here, to run once:
-pinMode(RPWM, OUTPUT);
-pinMode(LEN, OUTPUT);
-pinMode(REN, OUTPUT);
-digitalWrite(LEN, HIGH);
-digitalWrite(REN, HIGH);
+  pinMode(RPWM, OUTPUT);
+  pinMode(LEN, OUTPUT);
+  pinMode(REN, OUTPUT);
+  digitalWrite(LEN, HIGH);
+  digitalWrite(REN, HIGH);
 }
 void loop() {
-  // put your main code here, to run repeatedly:
-digitalWrite(RPWM, HIGH);
-delay(timeA);
-digitalWrite(RPWM, LOW);
-delay(timeA);
+  digitalWrite(RPWM, HIGH);
+  delay(timeA);
+  digitalWrite(RPWM, LOW);
+  delay(timeA);
 }
